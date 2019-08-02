8eb0a24f67f965b4d1dfe1c27d8407fd2a2c315c
zuofeng <zuofengnihao@163.com>
2019-08-02 17:03:49 +0800

    a

--- leetcode/src/main/java/com/takozy/test4/MedianOfTwoSortedArrays.java
+++ leetcode/src/main/java/com/takozy/test4/MedianOfTwoSortedArrays.java
@@ -27,14 +27,39 @@ package com.takozy.test4;
 public class MedianOfTwoSortedArrays {
 
     public static void main(String[] args) {
-        System.out.println(findMedianSortedArrays(new int[]{2,7,19,33}, new int[]{1,3,5,10,17}));
+        System.out.println(findMedianSortedArrays_1(new int[]{1,2}, new int[]{3,4}));
     }
 
+    /**
+     * 自己的思路 对比获取中位数 时间复杂度 O((m+n)/2)
+     * @param nums1
+     * @param nums2
+     * @return
+     */
     public static double findMedianSortedArrays(int[] nums1, int[] nums2) {
         int len = nums1.length + nums2.length;
-        int midPre = 0, minNext = 0;
-        for (int i = 0, j = 0, k = 0; k < len / 2; k ++) {
-            if ()
+        int midPre = 0, midNext = 0;
+        for (int i = 0, j = 0, k = 0; k <= len / 2; k ++) {
+            midPre = midNext;
+            if (i >= nums1.length) midNext = nums2[j++];
+            else if (j >= nums2.length) midNext = nums1[i++];
+            else if (nums1[i] < nums2[j]) midNext = nums1[i++];
+            else midNext = nums2[j++];
         }
+        return len % 2 == 0 ? (midPre + midNext) / 2.0 : midNext;
+    }
+
+    public static double findMedianSortedArrays_1(int[] nums1, int[] nums2) {
+        int m = nums1.length, n = nums2.length;
+        if (nums1.length > nums2.length) {
+            m = nums2.length; n = nums1.length;
+            int[] temp = nums1; nums1 = nums2; nums2 = temp;
+        }
+        int iMax = m, iMin = 0, half = (m + n + 1) / 2;
+        while (iMax >= iMin) {
+            int i = (iMax + iMin) / 2;
+            int j = half - i;
+        }
+        return 0.0;
     }
 }
