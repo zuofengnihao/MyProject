08a89ea32703157d66d0224717b812b529de4cc6
zuofeng <zuofengnihao@163.com>
2019-09-03 12:04:15 +0800

    v

--- leetcode/src/main/java/com/takozy/test4/MedianOfTwoSortedArrays.java
+++ leetcode/src/main/java/com/takozy/test4/MedianOfTwoSortedArrays.java
@@ -1,5 +1,7 @@
 package com.takozy.test4;
 
+import java.util.Date;
+
 /**
  * 给定两个大小为 m 和 n 的有序数组 nums1 和 nums2。
  *
@@ -27,7 +29,14 @@ package com.takozy.test4;
 public class MedianOfTwoSortedArrays {
 
     public static void main(String[] args) {
-        System.out.println(findMedianSortedArrays_1(new int[]{2,3}, new int[]{1}));
+        long t1 = new Date().getTime();
+        System.out.println(goodest(new int[]{1,2}, new int[]{3,4}));
+        long t2 = new Date().getTime();
+        System.out.println(t2 - t1);
+        long t3 = new Date().getTime();
+        System.out.println(findMedianSortedArrays_1(new int[]{1,2}, new int[]{3,4}));
+        long t4 = new Date().getTime();
+        System.out.println(t4 - t3);
     }
 
     /**
@@ -62,13 +71,75 @@ public class MedianOfTwoSortedArrays {
         int i , j, min = 0, max = m;
         while (min <= max) {
             i = (max + min) / 2;
-            j = (m + n) / 2 - i;
+            j = (m + n + 1) / 2 - i;
             if(i < max && a[i] < b[j-1]) {
-                max = i + 1;
+                min = i + 1;
             } else if (i > 0 && b[j] < a[i-1]) {
-                min = i - 1;
+                max = i - 1;
             } else {
+                int mid1;
+                if (i == 0) {
+                    mid1 = b[j-1];
+                } else if (j == 0) {
+                    mid1 = a[i-1];
+                } else {
+                    mid1 = Math.max(a[i-1], b[j-1]);
+                }
+                if ((a.length + b.length) % 2 == 1) return mid1;
 
+                int mid2;
+                if (i == m) {
+                    mid2 = b[j];
+                } else if (j == n) {
+                    mid2 = a[i];
+                } else {
+                    mid2 = Math.min(a[i], b[j]);
+                }
+                return ( mid1 + mid2 ) / 2.0D;
+            }
+        }
+        return 0.0;
+    }
+
+    public static double goodest(int[] nums1, int[] nums2) {
+        int m,n;
+        int[] a,b;
+        if (nums1.length > nums2.length) {
+            m = nums2.length; n = nums1.length;
+            a = nums2; b = nums1;
+        } else {
+            m = nums1.length; n = nums2.length;
+            a = nums1; b = nums2;
+        }
+        int i , j, min = 0, max = m;
+        while (min <= max) {
+            i = (max + min) / 2;
+            j = (m + n + 1) / 2 - i;
+            if(i < max && a[i] < b[j-1]) {
+                min = i + 1;
+            } else if (i > min && b[j] < a[i-1]) {
+                max = i - 1;
+            } else {
+                int left;
+                if (i == 0) {
+                    left = b[j-1];
+                } else if (j == 0) {
+                    left = a[i-1];
+                } else {
+                    left = Math.max(a[i-1],b[j-1]);
+                }
+                if ((m + n) % 2 == 1) return left;
+
+                int right;
+                if (i == m) {
+                    right = b[j];
+                } else if (j == n) {
+                    right = a[i];
+                } else {
+                    right = Math.min(a[i], b[j]);
+                }
+
+                return (left + right) / 2.0;
             }
         }
         return 0.0;
