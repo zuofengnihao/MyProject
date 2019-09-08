75f35460cbfb00bb8df801d8f0fa3d3bd6714f7f
左风 <zuofeng@zuofengdeMacBook-Pro.local>
2019-09-08 23:32:38 +0800

    mac

---
+++ leetcode/src/main/java/com/takozy/test21_linkedList/MergeTwoLists.java
@@ -0,0 +1,34 @@
+package com.takozy.test21_linkedList;
+
+/**
+ * 将两个有序链表合并为一个新的有序链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。 
+ *
+ * 示例：
+ *
+ * 输入：1->2->4, 1->3->4
+ * 输出：1->1->2->3->4->4
+ *
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/merge-two-sorted-lists
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class MergeTwoLists {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static ListNode mergeTwoLists (ListNode l1, ListNode l2) {
+        ListNode cur1 = l1;
+
+        
+        return l1;
+    }
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x){val = x;}
+}
