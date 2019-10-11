a34160628a0af046e873575d8dc17039e5dcade0
zuofeng <zuofengnihao@163.com>
2019-10-11 17:03:13 +0800

    com

--- leetcode/src/main/java/com/takozy/test51_recurrence/SolveNQueens.java
+++ leetcode/src/main/java/com/takozy/test51_recurrence/SolveNQueens.java
@@ -1,6 +1,10 @@
 package com.takozy.test51_recurrence;
 
+import sun.plugin2.gluegen.runtime.CPU;
+
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
 
 /**
@@ -34,35 +38,147 @@ import java.util.List;
  */
 public class SolveNQueens {
 
+    /**
+     * 循环行
+     *
+     * Q x x x      Q x x x             Q x x x       Q x x x
+     * x x . .  =>  x x Q x             x x x Q       x x x Q
+     * x . x .      x x x x  back ==>   x . x x  ==>  x Q x x  结束 无解
+     * x . . x      x . x x             x x . x       x x x x
+     *
+     * x Q x x      x Q x x      x Q x x      x Q x x
+     * x x x .  =>  x x x Q      x x x Q      x x x Q
+     * . x . x      . x x x  =>  Q x x x      Q x x x  解1 row+1 = n
+     * . x . .      . x . x      x x . x  =>  x x Q x
+     *
+     * x x Q x      x x Q x      x x Q x      x x Q x
+     * . x x x  =>  Q x x x      Q x x x      Q x x x
+     * x . x .      x x x .  =>  x x x Q      x x x Q  解2 row+1 = n
+     * . . x .      x . x .      x . x x  =>  x Q x x
+     *
+     * x x x Q      x x x Q      x x x Q                   x x x Q
+     * . . x x  =>  Q x x x      Q x x x                   x Q x x
+     * . x . x      x x . x  =>  x x Q x          back =>  x x x x  结束 无解
+     * x . . x      x . x x      x x x x  back =>          x x . x
+     *
+     * 上面图形为回溯思路
+     *
+     * 先确定怎样的才能不互相攻击到 即 皇后为中心的米子型格子都会被攻击到
+     * 首先可以确定 同row 只能有一个皇后 每次添加一个皇后后 递归行号row 即可 backtrack(int row)
+     * 与此皇后同列col不能再放皇后 使用 rows[] 数组 当row,col位置放置皇后
+     * rows[col]=1 判断是否同列 只需判断 rows[col] 此列是否为1
+     * 最后是皇后的两条对角线 我们发现 row + col 与 row - col 是两个常量 只要坐标 row,col 相加或相减等于
+     * 这两个常量中的一个 即在此皇后的对角线上 使用两个 HashSet 保存 这两个常量
+     *
+     *
+     * @param args
+     */
+
     public static void main(String[] args) {
-        List<List<String>> lists = solveNQeens(4);
-        System.out.println(lists);
+        SolveNQueens nQueens = new SolveNQueens();
+        List<List<String>> lists = nQueens.solveNQeens(8);
+        for (int i = 0; i < lists.size(); i++) {
+            System.out.println("方法" + (i+1) + ":");
+            for (int j = 0; j < lists.get(i).size(); j++) {
+                System.out.println(lists.get(i).get(j));
+            }
+            System.out.println();
+        }
     }
 
-    public static List<List<String>> solveNQeens(int n) {
-        List<List<String>> lists = new ArrayList<>();
-
-        List<String> item = new ArrayList<>();
-        addItem(0, false, n, "", item);
-
-        System.out.println(item);
-
-        return lists;
+    public List<List<String>> solveNQeens(int n) {
+        List<List<String>> result = new ArrayList<>();
+        int[] queens = new int[n];
+        int[] rows = new int[n];
+        HashSet<Integer> dale = new HashSet<>();
+        HashSet<Integer> hill = new HashSet<>();
+        backtrack(0, n, result, queens, dale, hill, rows);
+        return result;
     }
 
-    public static void addItem(int now, boolean flag, int n, String str, List<String> item) {
-        if (now >= n) {
-            item.add(str);
+    /**
+     * 递归行号 为每行添加queen
+     * @param row
+     * @param n
+     * @param result
+     * @param queens
+     * @param dale
+     * @param hill
+     * @param rows
+     */
+    public void backtrack(int row, int n, List<List<String>> result, int[] queens, HashSet<Integer> dale, HashSet<Integer> hill, int[] rows) {
+        //当row==n 说明已经放置好了n个皇后 将queens添加至结果集
+        if (row == n) {
+            List<String> item = new ArrayList<>();
+            for (int i = 0; i < n; i++) {
+                String str = "";
+                for (int j = 0; j < n; j++) {
+                    if (j == queens[i]) str += "Q";
+                    else str += ".";
+                }
+                item.add(str);
+            }
+            result.add(item);
             return;
         }
 
-        String yes = str + '.';
-        addItem(now+1, flag, n, yes, item);
-
-        if (!flag) {
-            String no = str + 'Q';
-            addItem(now+1, true, n, no, item);
+        //循环列号
+        for (int i = 0; i < n; i ++) {
+            //判断当前行列row,i 是否会被攻击 不会被攻击情况下继续逻辑
+            if (isNotUnderAttack(row, i, dale, hill, rows)) {
+                //放下皇后的方法
+                putDownQueen(row, i, dale, hill, queens, rows);
+                //递归调用下一行
+                backtrack(row + 1, n, result, queens, dale, hill, rows);
+                //移除刚才添加的皇后 让循环继续 查看此行row的下一个列i是否符合不被攻击
+                removeQueen(row, i, dale, hill, queens, rows);
+            }
         }
     }
 
+    /**
+     * 判断是否会被攻击
+     * @param row
+     * @param col
+     * @param dale
+     * @param hill
+     * @param rows
+     * @return
+     */
+    public boolean isNotUnderAttack(int row, int col, HashSet<Integer> dale, HashSet<Integer> hill, int[] rows) {
+        if (rows[col] == 0 && !dale.contains(row - col) && !hill.contains(row + col)) return true;
+        return false;
+    }
+
+    /**
+     * 放下皇后
+     * @param row
+     * @param col
+     * @param dale
+     * @param hill
+     * @param queens
+     * @param rows
+     */
+    public void putDownQueen(int row, int col, HashSet<Integer> dale, HashSet<Integer> hill, int[] queens, int[] rows) {
+        queens[row] = col;
+        rows[col] = 1;
+        dale.add(row - col);
+        hill.add(row + col);
+    }
+
+    /**
+     * 移除皇后
+     * @param row
+     * @param col
+     * @param dale
+     * @param hill
+     * @param queens
+     * @param rows
+     */
+    public void removeQueen(int row, int col, HashSet<Integer> dale, HashSet<Integer> hill, int[] queens, int[] rows) {
+        queens[row] = 0;
+        rows[col] = 0;
+        dale.remove(row - col);
+        hill.remove(row + col);
+    }
 }
